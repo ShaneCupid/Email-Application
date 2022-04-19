de525a619dca807b28cfe4d8ce93659faa6dc34d
Shane <shanecupid1@gmail.com>
2022-04-18 20:47:20 -0700

    Updates

--- src/emailApp/emailApp.java
+++ src/emailApp/emailApp.java
@@ -4,8 +4,7 @@ public class emailApp {
 
 	public static void main(String[] args) {
 Email  em1 = new Email("Will", "Smith");
-em1.setAlternateEmail("IloveJava@java.com");
-System.out.println(em1.getAlternateEmail());
+System.out.println(em1.showInfo());
 	}
 
 }
